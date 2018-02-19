6e1a0a7fc785451c74b8fe29c03c9e22f45bf67e
DemoUser <fnualkatomar@gmail.com>
2018-02-18 22:37:33 -0600

    Added a new file

--- gs-spring-boot/src/main/java/hello/JustToAdd.java
+++ gs-spring-boot/src/main/java/hello/JustToAdd.java
@@ -4,6 +4,7 @@ public class JustToAdd {
 
 	public static void main(String[] args) {
 		System.out.println("Hello this is my first Git Project Add File..");
+		System.out.println("Hello this is my first..");
 
 	}
 
