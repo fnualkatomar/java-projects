3beb086ff3a9de6c16e0ec69e7189c259ba6c2cc
DemoUser <fnualkatomar@gmail.com>
2018-02-21 21:20:39 -0600

    fl

--- gs-spring-boot/src/main/java/hello/Application.java
+++ gs-spring-boot/src/main/java/hello/Application.java
@@ -13,7 +13,7 @@ public class Application {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
-        System.out.println("Direct Commit");
+        System.out.println("Direct CommitFromLocal");
         System.out.println("Local Direct Commit11232");
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
