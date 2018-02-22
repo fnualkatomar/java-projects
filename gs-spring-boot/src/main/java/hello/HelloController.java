a26bbc464d83e175190147680ee347d1857eedca
DemoUser <fnualkatomar@gmail.com>
2018-02-21 21:42:59 -0600

    Added 777

--- gs-spring-boot/src/main/java/hello/HelloController.java
+++ gs-spring-boot/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-    	System.out.println("feature-123456666");
+    	System.out.println("feature-123456666777");
         return "Greetings from Spring Boot!";
     }
     
