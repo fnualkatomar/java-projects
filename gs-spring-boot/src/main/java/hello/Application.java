06780da0cdd85c150394a5e95e068670dd1446c3
DemoUser <fnualkatomar@gmail.com>
2018-02-21 21:16:39 -0600

    changed the file littlebit

--- gs-spring-boot/src/main/java/hello/Application.java
+++ gs-spring-boot/src/main/java/hello/Application.java
@@ -14,7 +14,7 @@ public class Application {
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
         System.out.println("Direct Commit");
-        System.out.println("Local Direct Commit1122");
+        System.out.println("Local Direct Commit11232");
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
