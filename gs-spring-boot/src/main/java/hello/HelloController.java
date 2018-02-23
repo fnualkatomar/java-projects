4fb6837ae6284b0596468aeeec6a07fb28bac432
DemoUser <fnualkatomar@gmail.com>
2018-02-23 16:35:42 -0600

    updated Master

--- gs-spring-boot/src/main/java/hello/HelloController.java
+++ gs-spring-boot/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-    	System.out.println("feature-123456666");
+    	System.out.println("feature-1234566669999");
         return "Greetings from Spring Boot!";
     }
     
